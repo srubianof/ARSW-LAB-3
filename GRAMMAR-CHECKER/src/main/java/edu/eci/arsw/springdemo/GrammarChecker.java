7b332803f11265bf415eb8e94705aac2dab9ec6e
Santiago <srubianof@icloud.com>
2020-08-26 13:34:50 -0500

    README part 1

--- GRAMMAR-CHECKER/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ GRAMMAR-CHECKER/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -10,29 +10,17 @@ public class GrammarChecker {
     @Autowired
 	@Qualifier("englishSpell")
 	SpellChecker sc;
-
     String x;
-
     public SpellChecker getSpellChecker() {
-
     	return sc;
     }
-
     public void setSpellChecker(SpellChecker sc) {
-
     	this.sc = sc;
     }
-
-
     public String check(String text) {
-
         StringBuffer sb = new StringBuffer();
         sb.append("Spell checking output:" + sc.checkSpell(text));
         sb.append("Plagiarism checking output: Not available yet");
-
         return sb.toString();
-
     }
-
-
 }
