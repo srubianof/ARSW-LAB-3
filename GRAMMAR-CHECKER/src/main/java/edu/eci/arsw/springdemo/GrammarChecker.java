c93643eeb93d65f67167db3bbcb1063ca2e479ac
Santiago <srubianof@icloud.com>
2020-08-26 13:24:43 -0500

    Terminando parte 1

--- GRAMMAR-CHECKER/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ GRAMMAR-CHECKER/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -1,34 +1,38 @@
 package edu.eci.arsw.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
 
+    @Autowired
+	@Qualifier("englishSpell")
 	SpellChecker sc;
 
-	String x;
+    String x;
 
-	public SpellChecker getSpellChecker() {
-		return sc;
-	}
+    public SpellChecker getSpellChecker() {
 
-	public void setSpellChecker(SpellChecker sc) {
-		this.sc = sc;
-	}
+    	return sc;
+    }
+
+    public void setSpellChecker(SpellChecker sc) {
+
+    	this.sc = sc;
+    }
 
 
-	public String check(String text){
-		
-		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
-		sb.append("Plagiarism checking output: Not available yet");
-		
-		
-		return sb.toString();
-		
-	}
-	
-	
+    public String check(String text) {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Spell checking output:" + sc.checkSpell(text));
+        sb.append("Plagiarism checking output: Not available yet");
+
+        return sb.toString();
+
+    }
+
+
 }
