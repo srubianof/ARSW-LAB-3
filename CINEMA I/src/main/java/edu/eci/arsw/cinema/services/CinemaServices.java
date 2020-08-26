d0e4c8e0dc7ed23d4dfbda0c5dc6279422c5c149
Santiago <srubianof@icloud.com>
2020-08-26 13:52:49 -0500

    Part2p1
    fin punto 1

--- CINEMA I/src/main/java/edu/eci/arsw/cinema/services/CinemaServices.java
+++ CINEMA I/src/main/java/edu/eci/arsw/cinema/services/CinemaServices.java
@@ -9,43 +9,47 @@ import edu.eci.arsw.cinema.model.Cinema;
 import edu.eci.arsw.cinema.model.CinemaFunction;
 import edu.eci.arsw.cinema.persistence.CinemaException;
 import edu.eci.arsw.cinema.persistence.CinemaPersitence;
+
 import java.util.List;
 import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 /**
- *
  * @author cristian
  */
+@Service
 public class CinemaServices {
     @Autowired
-    CinemaPersitence cps=null;
-    
-    public void addNewCinema(Cinema c){
-        
+    @Qualifier("cinemaPersistence")
+    CinemaPersitence cps = null;
+
+    public void addNewCinema(Cinema c) {
+
     }
-    
-    public Set<Cinema> getAllCinemas(){
+
+    public Set<Cinema> getAllCinemas() {
         return null;
     }
-    
+
     /**
-     * 
      * @param name cinema's name
      * @return the cinema of the given name created by the given author
      * @throws CinemaException
      */
-    public Cinema getCinemaByName(String name) throws CinemaException{
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public Cinema getCinemaByName(String name) throws CinemaException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    
-    public void buyTicket(int row, int col, String cinema, String date, String movieName){
-        throw new UnsupportedOperationException("Not supported yet."); 
+
+
+    public void buyTicket(int row, int col, String cinema, String date, String movieName) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     public List<CinemaFunction> getFunctionsbyCinemaAndDate(String cinema, String date) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 
