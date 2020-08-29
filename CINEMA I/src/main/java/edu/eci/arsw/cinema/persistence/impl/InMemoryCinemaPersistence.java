78effbabdb2053f3abdd1359d1cddd659be03704
Santiago <srubianof@icloud.com>
2020-08-29 16:34:17 -0500

    Punto 2 y 3 DONE

--- CINEMA I/src/main/java/edu/eci/arsw/cinema/persistence/impl/InMemoryCinemaPersistence.java
+++ CINEMA I/src/main/java/edu/eci/arsw/cinema/persistence/impl/InMemoryCinemaPersistence.java
@@ -5,61 +5,58 @@
  */
 package edu.eci.arsw.cinema.persistence.impl;
 
-import edu.eci.arsw.cinema.model.Cinema;
-import edu.eci.arsw.cinema.model.CinemaFunction;
-import edu.eci.arsw.cinema.model.Movie;
-import edu.eci.arsw.cinema.persistence.CinemaException;
-import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
-import edu.eci.arsw.cinema.persistence.CinemaPersitence;
+import edu.eci.arsw.cinema.model.*;
+import edu.eci.arsw.cinema.persistence.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- *
  * @author cristian
  */
 @Component
 @Qualifier("cinemaPersistence")
-public class InMemoryCinemaPersistence implements CinemaPersitence{
-    
-    private final Map<String,Cinema> cinemas=new HashMap<>();
+public class InMemoryCinemaPersistence implements CinemaPersitence {
+
+    private Map<String, Cinema> cinemas = new HashMap<>();
 
     public InMemoryCinemaPersistence() {
         //load stub data
-        String functionDate = "2018-12-18 15:30";
-        List<CinemaFunction> functions= new ArrayList<>();
-        CinemaFunction funct1 = new CinemaFunction(new Movie("SuperHeroes Movie","Action"),functionDate);
-        CinemaFunction funct2 = new CinemaFunction(new Movie("The Night","Horror"),functionDate);
-        functions.add(funct1);
-        functions.add(funct2);
-        Cinema c=new Cinema("cinemaX",functions);
-        cinemas.put("cinemaX", c);
-    }    
+//        String functionDate = "2018-12-18 15:30";
+//        List<CinemaFunction> functions = new ArrayList<>();
+//        CinemaFunction funct1 = new CinemaFunction(new Movie("SuperHeroes Movie", "Action"), functionDate);
+//        CinemaFunction funct2 = new CinemaFunction(new Movie("The Night", "Horror"), functionDate);
+//        functions.add(funct1);
+//        functions.add(funct2);
+//        Cinema c = new Cinema("cinemaX", functions);
+//        cinemas.put("cinemaX", c);
+    }
 
     @Override
     public void buyTicket(int row, int col, String cinema, String date, String movieName) throws CinemaException {
         Cinema cinema1 = this.cinemas.get(cinema);
+        boolean functionExists = false;
         List<CinemaFunction> functions = cinema1.getFunctions();
-        for (CinemaFunction function:functions){
-            if(function.getMovie().getName().equals(movieName) && function.getDate().equals(date)){
+        for (CinemaFunction function : functions) {
+            if (function.getMovie().getName().equals(movieName) && function.getDate().equals(date)) {
                 function.buyTicket(row, col);
+                functionExists = true;
             }
         }
-        //revisar si no hay funcion
+        if (!functionExists) {
+            throw new CinemaException("There are no functions that match either the title or the date");
+        }
     }
 
     @Override
     public List<CinemaFunction> getFunctionsbyCinemaAndDate(String cinema, String date) {
-        List<CinemaFunction> cinemaFunctionsMatched = null;
+        System.out.println(cinema+" "+date);
+        List<CinemaFunction> cinemaFunctionsMatched = new ArrayList<>();
         Cinema cinema1 = this.cinemas.get(cinema);
         List<CinemaFunction> cinemaFunctions = cinema1.getFunctions();
-        for (CinemaFunction cinemaFunction:cinemaFunctions){
-            if(cinemaFunction.getDate().equals(date)){
+        for (CinemaFunction cinemaFunction : cinemaFunctions) {
+            if (cinemaFunction.getDate().equals(date)) {
                 cinemaFunctionsMatched.add(cinemaFunction);
             }
         }
@@ -69,19 +66,28 @@ public class InMemoryCinemaPersistence implements CinemaPersitence{
 
     @Override
     public void saveCinema(Cinema c) throws CinemaPersistenceException {
-        if (cinemas.containsKey(c.getName())){
-            throw new CinemaPersistenceException("The given cinema already exists: "+c.getName());
+        if (cinemas.containsKey(c.getName())) {
+            throw new CinemaPersistenceException("The given cinema already exists: " + c.getName());
+        } else {
+            cinemas.put(c.getName(), c);
         }
-        else{
-            cinemas.put(c.getName(),c);
-        }   
     }
 
     @Override
-    public Cinema getCinema(String name) throws CinemaPersistenceException {
+    public Cinema getCinema(String name) {
         return cinemas.get(name);
     }
 
+    @Override
+    public Cinema getCinemaByName(String name){
+        return cinemas.get(name);
+    }
 
-
+    @Override
+    public Set<Cinema> getAllCinemas() throws CinemaException {
+        if (cinemas.isEmpty()) {
+            throw new CinemaException("There are no cinemas to show");
+        }
+        return new HashSet<>(cinemas.values());
+    }
 }
--- CINEMA I/src/main/java/edu/eci/arsw/cinema/services/CinemaServices.java
+++ CINEMA I/src/main/java/edu/eci/arsw/cinema/services/CinemaServices.java
@@ -8,6 +8,7 @@ package edu.eci.arsw.cinema.services;
 import edu.eci.arsw.cinema.model.Cinema;
 import edu.eci.arsw.cinema.model.CinemaFunction;
 import edu.eci.arsw.cinema.persistence.CinemaException;
+import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
 import edu.eci.arsw.cinema.persistence.CinemaPersitence;
 
 import java.util.List;
@@ -24,14 +25,14 @@ import org.springframework.stereotype.Service;
 public class CinemaServices {
     @Autowired
     @Qualifier("cinemaPersistence")
-    CinemaPersitence cps = null;
+    CinemaPersitence cps;
 
-    public void addNewCinema(Cinema c) {
-
+    public void addNewCinema(Cinema c) throws CinemaPersistenceException {
+        cps.saveCinema(c);
     }
 
-    public Set<Cinema> getAllCinemas() {
-        return null;
+    public Set<Cinema> getAllCinemas() throws CinemaException {
+        return cps.getAllCinemas();
     }
 
     /**
@@ -39,17 +40,17 @@ public class CinemaServices {
      * @return the cinema of the given name created by the given author
      * @throws CinemaException
      */
-    public Cinema getCinemaByName(String name) throws CinemaException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Cinema getCinemaByName(String name) throws CinemaPersistenceException {
+        return cps.getCinemaByName(name);
     }
 
 
-    public void buyTicket(int row, int col, String cinema, String date, String movieName) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void buyTicket(int row, int col, String cinema, String date, String movieName) throws CinemaException {
+        cps.buyTicket(row, col, cinema, date, movieName);
     }
 
     public List<CinemaFunction> getFunctionsbyCinemaAndDate(String cinema, String date) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return cps.getFunctionsbyCinemaAndDate(cinema, date);
     }
 
 
